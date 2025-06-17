5e6250a7f32e1bed76b757685a96dbc8d45bc78c
Dickson <dicksongodwin.dg@gmail.com>
2025-06-17 15:43:24 +0300

    onrender requirement

--- church_procurement/settings.py
+++ church_procurement/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-default-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True').lower() == 'true'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
